49f499fb76d874c029b72f3b936f5ecd2ca6faed
Flexer2010 <ovsyanov.andrey@mail.ru>
2020-05-25 20:01:29 +0300

    firstcommit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mathing;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Result(object sender, EventArgs e)
+        {
+            string error = "";
+            if (!(double.TryParse(textBox1.Text, out double a) && double.TryParse(textBox2.Text, out double b) && double.TryParse(textBox3.Text, out double c))) return; 
+            double[] result = QU.Result(
+                double.Parse(textBox1.Text),
+                double.Parse(textBox2.Text),
+                double.Parse(textBox3.Text), 
+                out error);
+            string resultStr = string.Join("\n", result);
+            MessageBox.Show(resultStr, error);
+        }
+    }
+}
